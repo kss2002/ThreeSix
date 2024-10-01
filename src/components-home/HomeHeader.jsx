f346f04d74bc77e69ea5ff7dba3410a2933f4e96
seong su kim <know.warehouse02@gmail.com>
2024-10-01 21:18:45 +0900

    헤더 1차 완성

--- src/components-home/HomeHeader.jsx
+++ src/components-home/HomeHeader.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const HomeHeader = () => {
-  return <div>HomeHeader</div>;
+  return (
+    <header id="Homeheader" role="banner">
+      <nav className="Homeheader__nav" role="navigation" aria-label="메인 메뉴">
+        <div className="Homeheader__logo">
+          <h3>삼식아</h3>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default HomeHeader;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 // style-scss 연동 (모든 scss 폴더를 가짐)
-import './assets/scss/style.scss';
+import './assets/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
