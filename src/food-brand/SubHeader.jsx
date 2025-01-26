0de01928a4840b4d01ead8181a6b46903ed01567
seong su kim <know.warehouse02@gmail.com>
2025-01-26 18:55:33 +0900

    서브 헤더 typeit Del

--- src/food-brand/SubHeader.jsx
+++ src/food-brand/SubHeader.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
-// TypeIt 라이브러리
-import TypeIt from 'typeit-react';
-
 const SubHeader = () => {
   const location = useLocation();
 
@@ -13,36 +10,7 @@ const SubHeader = () => {
   return (
     <header>
       <div id="mainScreen" className="SubHeader">
-        <div className="subHeader">
-          <TypeIt
-            getBeforeInit={(instance) => {
-              instance
-                .type('SAM SIKA')
-                .pause(700)
-                .delete(8)
-                .pause(600)
-                .type('하루를 맛있게')
-                .pause(700)
-                .delete(7)
-                .pause(600)
-                .type('삼식아')
-                .pause(1600);
-
-              return instance;
-            }}
-            options={{
-              cursor: false,
-              loop: true,
-              waitUntilVisible: true,
-              breakLines: true,
-
-              afterComplete: function (instance) {
-                instance.destroy();
-              },
-              //무한 루프 제어
-            }}
-          />
-        </div>
+        <div className="subHeader">삼식아</div>
 
         <nav>
           <ul className="Mainheader__nav">
--- src/main-components/MainFooter.jsx
+++ src/main-components/MainFooter.jsx
@@ -14,7 +14,9 @@ const MainFooter = () => {
             기재된 내용과 다를 수 있으며 자세한 사항은 가게에 문의해주세요!
           </p>
           <p className="Mainfooter__title">유니브 삼육대 1팀</p>
-          <h6 className="Mainfooter__update">업데이트:2025/01/14</h6>
+          <h6 className="Mainfooter__update">
+            Copyright &copy; 업데이트:2025/01/26
+          </h6>
         </div>
       </footer>
     </>
