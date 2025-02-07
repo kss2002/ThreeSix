1e49889154d74d6e4597342a56219c9fa138dc8a
seong su kim <know.warehouse02@gmail.com>
2025-02-07 21:24:12 +0900

    모바일 메인페이지 이미지 렌더링 속도 향상 1차 테스트

--- src/main-components/MainListMobile.jsx
+++ src/main-components/MainListMobile.jsx
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, { memo, useMemo, Suspense } from 'react';
 import { Map } from 'react-kakao-maps-sdk';
 import { Link } from 'react-router-dom';
 
-//사진 - moblie
-import w__port from '../assets/img/grid/w__port.png';
-import c__port from '../assets/img/grid/c__port.png';
-import k__port from '../assets/img/grid/k__port.png';
-import j__port from '../assets/img/grid/j__port.png';
-import s__port from '../assets/img/grid/s__port.png';
-import d__port from '../assets/img/grid/d__port.png';
-import f__port from '../assets/img/grid/f__port.png';
-
-// 메뉴 데이터를 배열로 구성
-const menuItems = [
-  { path: '/western', img: w__port, text: '양식' },
-  { path: '/chinese', img: c__port, text: '중식' },
-  { path: '/korean', img: k__port, text: '한식' },
-  { path: '/japanese', img: j__port, text: '일식' },
-  { path: '/school', img: s__port, text: '분식' },
-  { path: '/dessert', img: d__port, text: '후식' },
-  { path: '/fast', img: f__port, text: '기타' },
-];
-
+// 메뉴 데이터를 useMemo로 최적화
 const MainListMobile = () => {
+  const menuItems = useMemo(
+    () => [
+      {
+        path: '/western',
+        img: require('../assets/img/grid/w__port.webp'),
+        text: '양식',
+      },
+      {
+        path: '/chinese',
+        img: require('../assets/img/grid/c__port.webp'),
+        text: '중식',
+      },
+      {
+        path: '/korean',
+        img: require('../assets/img/grid/k__port.webp'),
+        text: '한식',
+      },
+      {
+        path: '/japanese',
+        img: require('../assets/img/grid/j__port.webp'),
+        text: '일식',
+      },
+      {
+        path: '/school',
+        img: require('../assets/img/grid/s__port.webp'),
+        text: '분식',
+      },
+      {
+        path: '/dessert',
+        img: require('../assets/img/grid/d__port.webp'),
+        text: '후식',
+      },
+      {
+        path: '/fast',
+        img: require('../assets/img/grid/f__port.webp'),
+        text: '기타',
+      },
+    ],
+    []
+  );
+
   return (
     <main>
       {/* 모바일 */}
@@ -31,14 +53,21 @@ const MainListMobile = () => {
           <div className="Mainlist__title">원하는 메뉴를 골라보세요!</div>
           <div className="Mainlist__menu">
             <div className="Mainlist__container">
-              {menuItems.map((item, index) => (
-                <Link key={index} to={item.path} rel="noopener noreferrer">
-                  <div className="main__grid-items">
-                    <img className="Main__img" src={item.img} alt={item.text} />
-                    <div className="Main__text">{item.text}</div>
-                  </div>
-                </Link>
-              ))}
+              <Suspense fallback={<div>Loading..</div>}>
+                {menuItems.map((item, index) => (
+                  <Link key={index} to={item.path} rel="noopener noreferrer">
+                    <div className="main__grid-items">
+                      <img
+                        className="Main__img"
+                        src={item.img}
+                        alt={item.text}
+                        loading="lazy"
+                      />
+                      <div className="Main__text">{item.text}</div>
+                    </div>
+                  </Link>
+                ))}
+              </Suspense>
             </div>
             <div id="research-map" className="Mainmap__title">
               지도로 식당 위치를 찾아보세요!
@@ -57,4 +86,4 @@ const MainListMobile = () => {
   );
 };
 
-export default MainListMobile;
+export default memo(MainListMobile);
