4a7a5201ed76b10b2c269b4d3cc2f3413a20396c
seong su kim <know.warehouse02@gmail.com>
2025-08-10 01:21:08 +0900

    new URL / meta.url 경로 수정

--- src/constants/PCdata.js
+++ src/constants/PCdata.js
@@ -2,7 +2,8 @@
 // 중식 이미지 데이터
 export const PCimagesChina = [
   {
-    src: require('../assets/img/pc-grid/china/마라탕.png'),
+    src: new URL('../assets/img/pc-grid/china/마라탕.png', import.meta.url)
+      .href,
     alt: '마라탕',
     title: '신룽푸마라탕',
     num: '전화번호 0507-1349-9044',
@@ -10,7 +11,7 @@ export const PCimagesChina = [
     path: '/sillung',
   },
   {
-    src: require('../assets/img/pc-grid/china/홍원.png'),
+    src: new URL('../assets/img/pc-grid/china/홍원.png', import.meta.url).href,
     alt: '홍원',
     title: '홍원',
     num: '전화번호 031-573-6155',
@@ -18,7 +19,8 @@ export const PCimagesChina = [
     path: '/hongwon',
   },
   {
-    src: require('../assets/img/pc-grid/china/맛차이나.png'),
+    src: new URL('../assets/img/pc-grid/china/맛차이나.png', import.meta.url)
+      .href,
     alt: '맛차이나',
     title: '맛차이나',
     num: '전화번호 0507-1454-8888',
@@ -26,7 +28,8 @@ export const PCimagesChina = [
     path: '/matchina',
   },
   {
-    src: require('../assets/img/pc-grid/china/중국관.png'),
+    src: new URL('../assets/img/pc-grid/china/중국관.png', import.meta.url)
+      .href,
     alt: '중국관',
     title: '중국관',
     num: '전화번호 031-528-8236',
@@ -34,7 +37,7 @@ export const PCimagesChina = [
     path: '/gawan',
   },
   {
-    src: require('../assets/img/rest.png'),
+    src: new URL('../assets/img/rest.png', import.meta.url).href,
     alt: '사진',
     title: '삼식아',
     num: '안녕하세요!',
@@ -45,7 +48,8 @@ export const PCimagesChina = [
 // 일식 이미지 데이터
 export const PCimagesJapan = [
   {
-    src: require('../assets/img/pc-grid/japan/별미가.png'),
+    src: new URL('../assets/img/pc-grid/japan/별미가.png', import.meta.url)
+      .href,
     alt: '별미가',
     title: '별미가',
     num: '전화번호 0507-1348-5845',
@@ -53,7 +57,8 @@ export const PCimagesJapan = [
     path: '/byeolmiga',
   },
   {
-    src: require('../assets/img/pc-grid/japan/토리코코로.png'),
+    src: new URL('../assets/img/pc-grid/japan/토리코코로.png', import.meta.url)
+      .href,
     alt: '토리코코로',
     title: '토리코코로',
     num: '전화번호 0507-1321-8794',
@@ -61,7 +66,8 @@ export const PCimagesJapan = [
     path: '/torikokoro',
   },
   {
-    src: require('../assets/img/pc-grid/japan/쇼쿠요쿠.png'),
+    src: new URL('../assets/img/pc-grid/japan/쇼쿠요쿠.png', import.meta.url)
+      .href,
     alt: '쇼쿠요쿠',
     title: '쇼쿠요쿠',
     num: '전화번호 031-527-2933',
@@ -69,7 +75,7 @@ export const PCimagesJapan = [
     path: '/syokuyoku',
   },
   {
-    src: require('../assets/img/rest.png'),
+    src: new URL('../assets/img/rest.png', import.meta.url).href,
     alt: '사진',
     title: '삼식아',
     num: '안녕하세요!',
@@ -80,7 +86,7 @@ export const PCimagesJapan = [
 // 양식 이미지 데이터
 export const PCimagesUS = [
   {
-    src: require('../assets/img/pc-grid/us/파스타.png'),
+    src: new URL('../assets/img/pc-grid/us/파스타.png', import.meta.url).href,
     alt: '오렌지몽키파스타/마르코죤스피자',
     title: '오렌지몽키/마르코죤스',
     num: '전화번호 0507-1378-0154',
@@ -88,7 +94,7 @@ export const PCimagesUS = [
     path: '/pastapizza',
   },
   {
-    src: require('../assets/img/pc-grid/us/도로시.png'),
+    src: new URL('../assets/img/pc-grid/us/도로시.png', import.meta.url).href,
     alt: '도로시화덕피자',
     title: '도로시화덕피자',
     num: '전화번호 031-527-7003',
@@ -96,7 +102,7 @@ export const PCimagesUS = [
     path: '/dorosi',
   },
   {
-    src: require('../assets/img/rest.png'),
+    src: new URL('../assets/img/rest.png', import.meta.url).href,
     alt: '사진',
     title: '삼식아',
     num: '안녕하세요!',
@@ -107,7 +113,8 @@ export const PCimagesUS = [
 // 한식 이미지 데이터
 export const PCimagesKorea = [
   {
-    src: require('../assets/img/pc-grid/Korea/출구없는.jpg'),
+    src: new URL('../assets/img/pc-grid/Korea/출구없는.jpg', import.meta.url)
+      .href,
     alt: '출구없는덮밥집',
     title: '출구없는덮밥집',
     num: '전화번호 010-9677-7518',
@@ -115,7 +122,10 @@ export const PCimagesKorea = [
     path: '/chulgu',
   },
   {
-    src: require('../assets/img/pc-grid/Korea/하늘지기, 꼬치.png'),
+    src: new URL(
+      '../assets/img/pc-grid/Korea/하늘지기, 꼬치.png',
+      import.meta.url
+    ).href,
     alt: '하늘지기(수내닭꼬치)',
     title: '하늘지기(수내닭꼬치)',
     num: '전화번호 010-4026-0631',
@@ -123,7 +133,10 @@ export const PCimagesKorea = [
     path: '/haneuljigi',
   },
   {
-    src: require('../assets/img/pc-grid/Korea/다람이 임자탕.png'),
+    src: new URL(
+      '../assets/img/pc-grid/Korea/다람이 임자탕.png',
+      import.meta.url
+    ).href,
     alt: '다람이임자탕',
     title: '다람이임자탕',
     num: '전화번호 031-529-9890',
@@ -131,7 +144,8 @@ export const PCimagesKorea = [
     path: '/darami',
   },
   {
-    src: require('../assets/img/pc-grid/Korea/닭갈비.png'),
+    src: new URL('../assets/img/pc-grid/Korea/닭갈비.png', import.meta.url)
+      .href,
     alt: '스마일닭갈비',
     title: '스마일닭갈비',
     num: '전화번호 010-7704-8286',
@@ -139,7 +153,8 @@ export const PCimagesKorea = [
     path: '/dalkkalbi',
   },
   {
-    src: require('../assets/img/pc-grid/Korea/청와삼대.png'),
+    src: new URL('../assets/img/pc-grid/Korea/청와삼대.png', import.meta.url)
+      .href,
     alt: '청와삼대',
     title: '청와삼대',
     num: '전화번호 031-528-0338',
@@ -147,7 +162,8 @@ export const PCimagesKorea = [
     path: '/cheone',
   },
   {
-    src: require('../assets/img/pc-grid/Korea/쌀국수.png'),
+    src: new URL('../assets/img/pc-grid/Korea/쌀국수.png', import.meta.url)
+      .href,
     alt: '쌀국수공방',
     title: '쌀국수공방',
     num: '전화번호 0507-1441-8582',
@@ -155,7 +171,8 @@ export const PCimagesKorea = [
     path: '/ssalguksu',
   },
   {
-    src: require('../assets/img/pc-grid/Korea/추어탕.png'),
+    src: new URL('../assets/img/pc-grid/Korea/추어탕.png', import.meta.url)
+      .href,
     alt: '담터추어탕',
     title: '담터추어탕',
     num: '전화번호 031-571-9544',
@@ -163,7 +180,8 @@ export const PCimagesKorea = [
     path: '/chueotang',
   },
   {
-    src: require('../assets/img/pc-grid/Korea/쭈꾸미.png'),
+    src: new URL('../assets/img/pc-grid/Korea/쭈꾸미.png', import.meta.url)
+      .href,
     alt: '담터쭈구미',
     title: '담터쭈구미',
     num: '전화번호 0507-1402-9544',
@@ -171,7 +189,8 @@ export const PCimagesKorea = [
     path: '/jjukkumi',
   },
   {
-    src: require('../assets/img/pc-grid/Korea/감자탕.png'),
+    src: new URL('../assets/img/pc-grid/Korea/감자탕.png', import.meta.url)
+      .href,
     alt: '세상만사감자탕',
     title: '세상만사감자탕',
     num: '전화번호 031-572-4998',
@@ -179,7 +198,8 @@ export const PCimagesKorea = [
     path: '/gamjatang',
   },
   {
-    src: require('../assets/img/pc-grid/Korea/칼국수.png'),
+    src: new URL('../assets/img/pc-grid/Korea/칼국수.png', import.meta.url)
+      .href,
     alt: '최고집해물찜칼국수',
     title: '최고집해물찜칼국수',
     num: '전화번호 031-529-3688',
@@ -187,7 +207,7 @@ export const PCimagesKorea = [
     path: '/choegojip',
   },
   {
-    src: require('../assets/img/rest.png'),
+    src: new URL('../assets/img/rest.png', import.meta.url).href,
     alt: '사진',
     title: '삼식아',
     num: '안녕하세요!',
@@ -198,7 +218,7 @@ export const PCimagesKorea = [
 // 분식 이미지 데이터
 export const PCimagesSchool = [
   {
-    src: require('../assets/img/pc-grid/school/꿈떡.png'),
+    src: new URL('../assets/img/pc-grid/school/꿈떡.png', import.meta.url).href,
     alt: '꿈꾸는 떡볶이',
     title: '꿈꾸는 떡볶이',
     num: '전화번호 031-527-7873',
@@ -206,7 +226,7 @@ export const PCimagesSchool = [
     path: '/kkum',
   },
   {
-    src: require('../assets/img/pc-grid/school/마떡.png'),
+    src: new URL('../assets/img/pc-grid/school/마떡.png', import.meta.url).href,
     alt: '마녀 떡볶이',
     title: '마녀 떡볶이',
     num: '전화번호 0507-1329-9769',
@@ -214,7 +234,7 @@ export const PCimagesSchool = [
     path: '/manyeo',
   },
   {
-    src: require('../assets/img/rest.png'),
+    src: new URL('../assets/img/rest.png', import.meta.url).href,
     alt: '사진',
     title: '삼식아',
     num: '안녕하세요!',
@@ -225,7 +245,10 @@ export const PCimagesSchool = [
 // 후식 이미지 데이터
 export const PCimagesDessert = [
   {
-    src: require('../assets/img/pc-grid/dessert/스마일 하우스.png'),
+    src: new URL(
+      '../assets/img/pc-grid/dessert/스마일 하우스.png',
+      import.meta.url
+    ).href,
     alt: '스마일하우스',
     title: '스마일하우스',
     num: '전화번호 031-527-3664',
@@ -233,7 +256,8 @@ export const PCimagesDessert = [
     path: '/seumail',
   },
   {
-    src: require('../assets/img/pc-grid/dessert/베이커리.png'),
+    src: new URL('../assets/img/pc-grid/dessert/베이커리.png', import.meta.url)
+      .href,
     alt: '바글바글 베이커리',
     title: '바글바글 베이커리',
     num: '전화번호 0507-1415-0208',
@@ -241,7 +265,10 @@ export const PCimagesDessert = [
     path: '/bageul',
   },
   {
-    src: require('../assets/img/pc-grid/dessert/매머드커피.png'),
+    src: new URL(
+      '../assets/img/pc-grid/dessert/매머드커피.png',
+      import.meta.url
+    ).href,
     alt: '매머드 커피',
     title: '매머드 커피',
     num: '전화번호 0505-042-0428',
@@ -249,7 +276,10 @@ export const PCimagesDessert = [
     path: '/maemeodeu',
   },
   {
-    src: require('../assets/img/pc-grid/dessert/린스테이블.png'),
+    src: new URL(
+      '../assets/img/pc-grid/dessert/린스테이블.png',
+      import.meta.url
+    ).href,
     alt: '린스테이블',
     title: '린스테이블',
     num: '전화번호 070-8736-3679',
@@ -257,7 +287,8 @@ export const PCimagesDessert = [
     path: '/rinseu',
   },
   {
-    src: require('../assets/img/pc-grid/dessert/이디야.png'),
+    src: new URL('../assets/img/pc-grid/dessert/이디야.png', import.meta.url)
+      .href,
     alt: '이디야',
     title: '이디야',
     num: '전화번호 0507-1397-0208',
@@ -265,7 +296,10 @@ export const PCimagesDessert = [
     path: '/idiya',
   },
   {
-    src: require('../assets/img/pc-grid/dessert/YOGI YOGURT.png'),
+    src: new URL(
+      '../assets/img/pc-grid/dessert/YOGI YOGURT.png',
+      import.meta.url
+    ).href,
     alt: 'YOGI YOGURT',
     title: 'YOGI YOGURT',
     num: '전화번호 모름',
@@ -273,7 +307,8 @@ export const PCimagesDessert = [
     path: '/yogi',
   },
   {
-    src: require('../assets/img/pc-grid/dessert/카페 공강.png'),
+    src: new URL('../assets/img/pc-grid/dessert/카페 공강.png', import.meta.url)
+      .href,
     alt: '카페 공강',
     title: '카페 공강',
     num: '전화번호 031-575-0908',
@@ -281,7 +316,10 @@ export const PCimagesDessert = [
     path: '/gonggang',
   },
   {
-    src: require('../assets/img/pc-grid/dessert/하이오커피.png'),
+    src: new URL(
+      '../assets/img/pc-grid/dessert/하이오커피.png',
+      import.meta.url
+    ).href,
     alt: '하이오커피',
     title: '하이오커피',
     num: '전화번호 031-527-7555',
@@ -289,7 +327,7 @@ export const PCimagesDessert = [
     path: '/haio',
   },
   {
-    src: require('../assets/img/rest.png'),
+    src: new URL('../assets/img/rest.png', import.meta.url).href,
     alt: '사진',
     title: '삼식아',
     num: '안녕하세요!',
@@ -300,7 +338,7 @@ export const PCimagesDessert = [
 // 기타 이미지 데이터
 export const PCimagesRest = [
   {
-    src: require('../assets/img/pc-grid/rest/후참잘.png'),
+    src: new URL('../assets/img/pc-grid/rest/후참잘.png', import.meta.url).href,
     alt: '후라이드 참 잘하는 집',
     title: '후라이드 참 잘하는 집',
     num: '전화번호 031-527-2509',
@@ -308,7 +346,8 @@ export const PCimagesRest = [
     path: '/huchamjal',
   },
   {
-    src: require('../assets/img/pc-grid/rest/직진닭강정.png'),
+    src: new URL('../assets/img/pc-grid/rest/직진닭강정.png', import.meta.url)
+      .href,
     alt: '직진 닭강정',
     title: '직진 닭강정',
     num: '전화번호 0507-1320-1892',
@@ -316,7 +355,7 @@ export const PCimagesRest = [
     path: '/jikjin',
   },
   {
-    src: require('../assets/img/pc-grid/rest/맘터.png'),
+    src: new URL('../assets/img/pc-grid/rest/맘터.png', import.meta.url).href,
     alt: '맘스터치',
     title: '맘스터치',
     num: '전화번호 031-572-0572',
@@ -324,7 +363,7 @@ export const PCimagesRest = [
     path: '/moms',
   },
   {
-    src: require('../assets/img/pc-grid/rest/샌두.png'),
+    src: new URL('../assets/img/pc-grid/rest/샌두.png', import.meta.url).href,
     alt: '샌두',
     title: '샌두',
     num: '전화번호 031-575-5558',
@@ -332,7 +371,7 @@ export const PCimagesRest = [
     path: '/saendu',
   },
   {
-    src: require('../assets/img/pc-grid/rest/부리또.png'),
+    src: new URL('../assets/img/pc-grid/rest/부리또.png', import.meta.url).href,
     alt: '창부리또',
     title: '창부리또',
     num: '전화번호 0507-1488-3848',
@@ -340,7 +379,7 @@ export const PCimagesRest = [
     path: '/chang',
   },
   {
-    src: require('../assets/img/rest.png'),
+    src: new URL('../assets/img/rest.png', import.meta.url).href,
     alt: '사진',
     title: '삼식아',
     num: '안녕하세요!',
